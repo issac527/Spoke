e4c6efb738f859df34b90f26be4104379797cb84
lim isac <lim_527@naver.com>
2022-02-22 15:56:45 +0900

    Custom Elements - SpinningCube Rotate Add

--- src/editor/nodes/SpinningCubeNode.js
+++ src/editor/nodes/SpinningCubeNode.js
@@ -1,4 +1,5 @@
-import { Mesh, MeshBasicMaterial, BoxBufferGeometry } from "three";
+// Note we'll need to import the Math class from three, but it conflicts with the browser's Math class so we'll map it to MathUtils
+import { Mesh, MeshBasicMaterial, BoxBufferGeometry, Euler, Math as MathUtils } from "three";
 import EditorNodeMixin from "./EditorNodeMixin";
 
 export default class SpinningCubeNode extends EditorNodeMixin(Mesh) {
@@ -6,13 +7,55 @@ export default class SpinningCubeNode extends EditorNodeMixin(Mesh) {
 
   static nodeName = "Spinning Cube";
 
+  // When we load the Spoke project we need to load this new speed property, so we'll need a custom deserialize function.
+  static async deserialize(editor, json) {
+    // The default deserializer will set the transform, name, visibility, and other basic properties.
+    const node = await super.deserialize(editor, json);
+
+    // json will be the entity object in the Spoke project and components will be an array of the form:
+    // { name: "spinning-cube", props: { speed: 10 } }
+
+    const { speed } = json.components.find(c => c.name === SpinningCubeNode.componentName).props;
+
+    // We'll set the object's speed property and default to 10 deg/s if it's undefined.
+    node.speed = speed === undefined ? 10 : speed;
+
+    return node;
+  }
+
   constructor(editor) {
     super(editor, new BoxBufferGeometry(), new MeshBasicMaterial());
+
+    // We'll set the default speed to rotate 10 degrees every second.
+    this.speed = 10;
+
+    this.originalRotation = new Euler();
+  }
+
+  onPlay() {
+    this.originalRotation.copy(this.rotation);
+  }
+
+  onUpdate(dt) {
+    if (this.editor.playing) {
+      // Now we can use the speed property
+      // Note: rotation.y is in radians so we'll need to convert from degrees to radians
+      this.rotation.y += dt * MathUtils.degToRad(this.speed);
+    }
+  }
+
+  onPause() {
+    this.rotation.copy(this.originalRotation);
   }
 
   serialize() {
     return super.serialize({
-      [SpinningCubeNode.componentName]: {}
+      [SpinningCubeNode.componentName]: {
+        // We need to serialize this new property to the Spoke project.
+        // The contents of this object will get set on the entity's "spinning-node" component
+        // { name: "spinning-cube", props: { speed: <speed> } }
+        speed: this.speed
+      }
     });
   }
 }
--- src/ui/properties/SpinningCubeNodeEditor.js
+++ src/ui/properties/SpinningCubeNodeEditor.js
@@ -1,11 +1,47 @@
 import React from "react";
 import NodeEditor from "./NodeEditor";
 import { Cube } from "styled-icons/fa-solid/Cube";
+// Add the following imports
+import PropTypes from "prop-types";
+import NumericInputGroup from "../inputs/NumericInputGroup";
+import useSetPropertySelected from "./useSetPropertySelected";
 
 export default function SpinningCubeNodeEditor(props) {
-  return <NodeEditor {...props} description={SpinningCubeNodeEditor.description} />;
+  // We're passing all of the props to NodeEditor, but we also want to pull out editor and node.
+  const { editor, node } = props;
+  // useSetPropertySelected is a hook that can be used to set the specified properties on the selected objects.
+  const onChangeSpeed = useSetPropertySelected(editor, "speed");
+
+  // Here's a rundown of the NumericInputGroup props:
+  // name: The input label
+  // smallStep: How much the value adjusts when you hold ctrl + up/down arrows
+  // mediumStep: How much the value adjusts when you press the up/down arrows
+  // largeStep: How much the value adjusts when you hold shift + up/down arrows
+  // value: The speed property of the current active node (last selected)
+  // onChange: The setter from above
+  // unit: Show the correct units to the user
+
+  return (
+    <NodeEditor {...props} description={SpinningCubeNodeEditor.description}>
+      <NumericInputGroup
+        name="Speed"
+        smallStep={0.1}
+        mediumStep={1}
+        largeStep={10}
+        value={node.speed}
+        onChange={onChangeSpeed}
+        unit="°/s"
+      />
+    </NodeEditor>
+  );
 }
 
 SpinningCubeNodeEditor.iconComponent = Cube;
 
-SpinningCubeNodeEditor.description = "It's a cube! And it spins!";
+SpinningCubeNodeEditor.description = "It's a cube! And it spins!!!";
+
+// Make eslint happy with some prop types.
+SpinningCubeNodeEditor.propTypes = {
+  editor: PropTypes.object.isRequired,
+  node: PropTypes.object.isRequired
+};
